3893f3ad29e95116a58623ceea1f9429a43099bd
olaolaabim <117292918+olaolaabim@users.noreply.github.com>
2022-11-04 12:40:10 +0000

    Add files via upload

---
+++ strings.py
@@ -0,0 +1,18 @@
+name = "Mike"
+age = "47"
+print("my name  " + name + ",")
+name = "Jike"
+print(name + " is a big boy")
+print("He is \"" + age + " years old")
+phrase = "Giraffe Academy"
+print(phrase.replace("G", 'P'))
+print((phrase.lower()))
+print(phrase.upper())
+print(phrase.isupper())
+print(phrase.find('a'))#finds the index of a string
+print(phrase.count('a'))#counts the occurence of a char
+print(len(phrase))#counting the no of char in a string
+print(phrase[4])#printing the 3rd index
+
+
+
